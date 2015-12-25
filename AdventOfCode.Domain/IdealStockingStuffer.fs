455b84257f8419069fc7d97b80026629e8ef6df0
selketjah <gien.verschatse@gmail.com>
2015-12-25 11:35:15 +0100

    add md5 hash implementation and test with given examples

--- AdventOfCode.Domain/IdealStockingStuffer.fs
+++ AdventOfCode.Domain/IdealStockingStuffer.fs
@@ -1,5 +1,17 @@
 ﻿namespace AdventOfCode
 
-module IdealStockingStuffer =
-  let input = "yzbqklnj"
+open System.Security.Cryptography
+open System.Text
 
+module IdealStockingStuffer =
+  let input = "yzbqklnj"B
+
+  let testInput = "pqrstuv1048970"B
+
+  let md5 (data : byte array) : string =
+      use md5 = MD5.Create()
+      (StringBuilder(), md5.ComputeHash(data))
+      ||> Array.fold (fun sb b -> sb.Append(b.ToString("x2")))
+      |> string
+
+  let hash = md5 input;
