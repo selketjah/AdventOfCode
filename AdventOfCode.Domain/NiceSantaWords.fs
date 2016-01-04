2c6c0e4a4c8fe7432a52b2b546af240cc5be4cec
selketjah <gien.verschatse@gmail.com>
2016-01-04 17:47:07 +0100

    added pattern matching for excluded combinations

--- AdventOfCode.Domain/NiceSantaWords.fs
+++ AdventOfCode.Domain/NiceSantaWords.fs
@@ -7,3 +7,10 @@ module NiceSantaWords =
   let loadFile =
     "B:/PROJECTS/AdventOfCode/AdventOfCode.Domain/inputs/santawords.txt"
     |> File.ReadAllLines
+
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+
+  let matchPattern word =
+    match word with
+    | "ab" | "cd" | "pq" | "xy" -> false
+    | _ -> true
