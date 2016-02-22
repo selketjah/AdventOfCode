15d57772446290044b609a203e141c6353d16872
Gien Verschatse <gverschatse@csc.com>
2016-02-22 14:38:57 +0100

    floors in csharp

---
+++ AdventOfCode.CSharp.Run/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode.CSharp.Run {
+  class Program {
+    static void Main(string[] args) {
+
+      Console.WriteLine(new SantasFloor().EndFloor());
+      Console.WriteLine(new SantasFloor().PositionFirstTimeInBasement());
+      Console.Read();
+    }
+  }
+}
---
+++ AdventOfCode.CSharp/SantasFloor.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.CSharp
+{
+  public class SantasFloor
+  {
+
+    public string FloorInstructions()
+    {
+      return File.ReadAllText(@"E:\labdays\AdventOfCode\AdventOfCode.CSharp\inputs\floors.txt");
+    }
+
+    public int EndFloor()
+    {
+      return FloorInstructions().Sum(f => MapFloor(f));
+    }
+
+    public int MapFloor(char c)
+    {
+      switch (c)
+      {
+        case '(':
+          return 1;
+        case ')':
+          return -1;
+        default:
+          return 0;
+      }
+    }
+  }
+}
